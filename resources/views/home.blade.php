f7dc735cf3324d417e1eaddf5a7cb57e90c53ff0
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-15 19:01:42 +0100

    style in main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,33 @@
 @extends('layout.main')
+
+@section('content')
+
+<!-- Jumbotron  -->
+<div class="jumbotron">
+    <figure>
+        <img src="{{ asset('images/jumbotron.jpg') }}" alt="logo jumbotron">
+    </figure>
+</div>
+
+<!-- Section Main-Series  -->
+<div class="main-series">
+    <div class="container">
+        <CardImmage />
+        <div class="load"><button>LOAD MORE</button></div>
+    </div>
+</div>
+
+<section id="shop">
+    <div class="container">
+        <div class="card-img">
+            <figure>
+                <div class="immagine">
+                    <img src="" alt="card.description" />
+                </div>
+                <div class="text">Testo</div>
+            </figure>
+        </div>
+    </div>
+</section>
+
+@endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -13,33 +13,7 @@
     @include('includes.header')
 
     <main>
-        <!-- Jumbotron  -->
-        <div class="jumbotron">
-            <div class="container">
-                <button>CURRENT SERIES</button>
-            </div>
-        </div>
-
-        <!-- Section Main-Series  -->
-        <div class="main-series">
-            <div class="container">
-                <CardImmage />
-                <div class="load"><button>LOAD MORE</button></div>
-            </div>
-        </div>
-
-        <section id="shop">
-            <div class="container">
-                <div class="card-img">
-                    <figure>
-                        <div class="immagine">
-                            <img src="" alt="card.description" />
-                        </div>
-                        <div class="text">Testo</div>
-                    </figure>
-                </div>
-            </div>
-        </section>
+        @yield('content')
     </main>
 
     <footer>
